c66884f68f6ad6b6fd329ee943e9c36e0835244c
sadamhabnawi28 <145916686+sadamhabnawi28@users.noreply.github.com>
2025-10-08 09:03:15 +0700

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,3 +1,14 @@
+/*
+===============================================================================
+DDL Script: Create Silver Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'silver' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
 drop table if exists silver.crm_cust_info;
 
 create table silver.crm_cust_info (
