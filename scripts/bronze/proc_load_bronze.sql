fe3bb32e5c687ced64c3cc1574ec20b861750be3
sadamhabnawi28 <145916686+sadamhabnawi28@users.noreply.github.com>
2025-10-08 09:34:46 +0700

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -10,49 +10,119 @@ Script Purpose:
 ===============================================================================
 */
 
-TRUNCATE TABLE bronze.crm_cust_info;
+CREATE OR REPLACE PROCEDURE bronze.load_bronze()
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    start_time TIMESTAMP;
+    end_time TIMESTAMP;
+    batch_start_time TIMESTAMP;
+    batch_end_time TIMESTAMP;
+BEGIN
+    batch_start_time := NOW();
+    RAISE NOTICE '================================================';
+    RAISE NOTICE 'Loading Bronze Layer';
+    RAISE NOTICE '================================================';
 
-COPY bronze.crm_cust_info
-FROM 'D:\Data Project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-DELIMITER ',' 
-CSV HEADER;
+    -------------------------------------------------------------------------
+    -- Load CRM Tables
+    -------------------------------------------------------------------------
+    RAISE NOTICE '------------------------------------------------';
+    RAISE NOTICE 'Loading CRM Tables';
+    RAISE NOTICE '------------------------------------------------';
 
+    -- crm_cust_info
+    start_time := NOW();
+    RAISE NOTICE '>> Truncating Table: bronze.crm_cust_info';
+    TRUNCATE TABLE bronze.crm_cust_info;
+    RAISE NOTICE '>> Inserting Data Into: bronze.crm_cust_info';
+    COPY bronze.crm_cust_info
+    FROM 'D:\Data Project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+    DELIMITER ',' CSV HEADER;
+    end_time := NOW();
+    RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(EPOCH FROM (end_time - start_time));
 
-TRUNCATE TABLE bronze.crm_prd_info;
+    -- crm_prd_info
+    start_time := NOW();
+    RAISE NOTICE '>> Truncating Table: bronze.crm_prd_info';
+    TRUNCATE TABLE bronze.crm_prd_info;
+    RAISE NOTICE '>> Inserting Data Into: bronze.crm_prd_info';
+    COPY bronze.crm_prd_info
+    FROM 'D:\Data Project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+    DELIMITER ',' CSV HEADER;
+    end_time := NOW();
+    RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(EPOCH FROM (end_time - start_time));
 
-COPY bronze.crm_prd_info
-FROM 'D:\Data Project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-DELIMITER ',' 
-CSV HEADER;
+    -- crm_sales_details
+    start_time := NOW();
+    RAISE NOTICE '>> Truncating Table: bronze.crm_sales_details';
+    TRUNCATE TABLE bronze.crm_sales_details;
+    RAISE NOTICE '>> Inserting Data Into: bronze.crm_sales_details';
+    COPY bronze.crm_sales_details
+    FROM 'D:\Data Project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+    DELIMITER ',' CSV HEADER;
+    end_time := NOW();
+    RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(EPOCH FROM (end_time - start_time));
 
-TRUNCATE TABLE bronze.crm_sales_details;
+    -------------------------------------------------------------------------
+    -- Load ERP Tables
+    -------------------------------------------------------------------------
+    RAISE NOTICE '------------------------------------------------';
+    RAISE NOTICE 'Loading ERP Tables';
+    RAISE NOTICE '------------------------------------------------';
 
-COPY bronze.crm_sales_details
-FROM 'D:\Data Project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-DELIMITER ',' 
-CSV HEADER;
+    -- erp_loc_a101
+    start_time := NOW();
+    RAISE NOTICE '>> Truncating Table: bronze.erp_loc_a101';
+    TRUNCATE TABLE bronze.erp_loc_a101;
+    RAISE NOTICE '>> Inserting Data Into: bronze.erp_loc_a101';
+    COPY bronze.erp_loc_a101
+    FROM 'D:\Data Project\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+    DELIMITER ',' CSV HEADER;
+    end_time := NOW();
+    RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(EPOCH FROM (end_time - start_time));
 
-TRUNCATE TABLE bronze.erp_cust_az12;
+    -- erp_cust_az12
+    start_time := NOW();
+    RAISE NOTICE '>> Truncating Table: bronze.erp_cust_az12';
+    TRUNCATE TABLE bronze.erp_cust_az12;
+    RAISE NOTICE '>> Inserting Data Into: bronze.erp_cust_az12';
+    COPY bronze.erp_cust_az12
+    FROM 'D:\Data Project\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+    DELIMITER ',' CSV HEADER;
+    end_time := NOW();
+    RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(EPOCH FROM (end_time - start_time));
 
-COPY bronze.erp_cust_az12
-FROM 'D:\Data Project\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
-DELIMITER ',' 
-CSV HEADER;
+    -- erp_px_cat_g1v2
+    start_time := NOW();
+    RAISE NOTICE '>> Truncating Table: bronze.erp_px_cat_g1v2';
+    TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+    RAISE NOTICE '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
+    COPY bronze.erp_px_cat_g1v2
+    FROM 'D:\Data Project\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+    DELIMITER ',' CSV HEADER;
+    end_time := NOW();
+    RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(EPOCH FROM (end_time - start_time));
 
-TRUNCATE TABLE bronze.erp_loc_a101;
+    -------------------------------------------------------------------------
+    -- Completion
+    -------------------------------------------------------------------------
+    batch_end_time := NOW();
+    RAISE NOTICE '==========================================';
+    RAISE NOTICE 'Loading Bronze Layer is Completed';
+    RAISE NOTICE '   - Total Load Duration: % seconds', EXTRACT(EPOCH FROM (batch_end_time - batch_start_time));
+    RAISE NOTICE '==========================================';
 
-COPY bronze.erp_loc_a101
-FROM 'D:\Data Project\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
-DELIMITER ',' 
-CSV HEADER;
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE NOTICE '==========================================';
+        RAISE NOTICE 'ERROR OCCURRED DURING LOADING BRONZE LAYER';
+        RAISE NOTICE 'Error Message: %', SQLERRM;
+        RAISE NOTICE '==========================================';
+END;
+$$;
 
-
-TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-
-COPY bronze.erp_px_cat_g1v2
-FROM 'D:\Data Project\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
-DELIMITER ',' 
-CSV HEADER;
+CALL bronze.load_bronze();
 
 
 
